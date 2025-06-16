f86094a49dac14d846628a8cfe9f3ca66da9182c
Jonathan Sada <jonathansada@gmail.com>
2025-06-16 09:00:15 +0200

    Improved image cropping, label balancer, deterministic models. Changed transfer layers model depth. Implemented fine-tunning for MobileNetV2.

--- helpers.py
+++ helpers.py
@@ -34,31 +34,24 @@ def describe_complete(df, skipna=False):
 
     return stats_df
 
-def get_img_xy(df, path_column, label_column, img_width=28, img_height=28, color_mode="rgb"):
+def get_img_xy(df, path_column, label_column, img_width=28, img_height=28, color_mode="rgb", seed=None, 
+               keep_aspect_ratio = True, random_flip=False, random_brightness=False, random_contrast=False, random_hue=False, random_saturation=False):
+    set_seed(seed)
     # Prepare X
     images = []
     for index, row in df.iterrows():
-        # Automatic Resizing
-        #images.append(
-        #    tf.keras.utils.img_to_array(
-        #        tf.keras.utils.load_img(row[path_column], target_size=(img_height, img_width), keep_aspect_ratio=True, interpolation="bicubic", color_mode=color_mode)))
-
-        # Manual Resizing
-        img = tf.keras.utils.load_img(row[path_column])
-        # WIP: Resize logic improvement for non-squared dimensions
-        #height_rs = img_height if img_width < img_height else int(math.ceil(img_width * img.height / img.width))
-        #width_rs  = img_width  if img_width > img_height else int(math.ceil(img_height * img.width / img.height))
-        # Define Resize Height (Only working with squared images)
-        width_rs  = int(img_height * img.width/img.height) if img.width > img.height else img_width
-        height_rs = int(img_width * img.height/img.width) if img.width < img.height else img_height
-        image_array = tf.image.resize(img, size=(height_rs, width_rs), method="nearest", preserve_aspect_ratio=False)
-        # Crop no offset on purpose
-        offset_height = int((height_rs - img_height) / 2) if height_rs > img_height else 0
-        offset_width  = int((width_rs - img_width) / 2)if width_rs > img_width else 0
-        image_array = tf.image.crop_to_bounding_box(image_array, offset_height=offset_height, offset_width=offset_width, target_height=img_height, target_width=img_width) 
-
-        images.append(tf.keras.preprocessing.image.array_to_img(image_array))
-        
+        image_array = tf.keras.utils.img_to_array(tf.keras.utils.load_img(row[path_column], target_size=(img_height, img_width), keep_aspect_ratio=keep_aspect_ratio, color_mode=color_mode))
+        if random_flip:
+            image_array = tf.image.random_flip_left_right(image_array, seed=seed)
+        if random_brightness:
+            image_array = tf.image.random_brightness(image_array, 0.25, seed=seed)
+        if random_contrast and seed == None:
+            image_array = tf.image.random_contrast(image_array, 0.25, 0.75)
+        if random_hue:
+            image_array = tf.image.random_hue (image_array, 0.25, seed=seed)
+        if random_saturation and image_array.shape[2] == 3: 
+            image_array = tf.image.random_saturation(image_array, 0.25, 0.75, seed=seed)
+        images.append(image_array)
     X = np.array(images)
     
     # Prepare y
@@ -133,24 +126,39 @@ def cat_analisis(df, column, log=False, top=False, bottom=False, chart_size=Fals
         if bottom:
             display(frequency_table.tail(bottom))
 
-def balance_labels(df, label_colum):
+def balance_labels(df, label_colum, shuffle = False, seed=None):
     subset = []
     freq_table = getFrequencyTable(df, label_colum)
-    for label in freq_table.index:
-        subset.append(df[df[label_colum] == label][:freq_table["count"].min()])
+    if shuffle:
+        subset = [df[df[label_colum] == label].sample(n = freq_table["count"].min(), random_state=seed) for label in freq_table.index]
+    else:
+        subset = [df[df[label_colum] == label][:freq_table["count"].min()] for label in freq_table.index]
+        
     df = pd.concat(subset)
     df.sort_index(inplace=True)
     return df
 
-def preprocess_data(df, img_width, img_height, path_column, label_column, normalizer=False, seed=None):
+def set_seed(seed=None):
     if seed != None:
+        np.random.seed(seed)
+        tf.random.set_seed(seed)
         tf.keras.utils.set_random_seed(seed)
+        tf.config.experimental.enable_op_determinism()
+
+
+def preprocess_data(df, img_width, img_height, path_column, label_column, normalizer=False, seed=None, validation_set = False,
+                    keep_aspect_ratio = True, random_flip=False, random_brightness=False, random_contrast=False, random_hue=False, random_saturation=False):
+    set_seed(seed)
 
     # Split DataSets
-    X, y, class_names = get_img_xy(df = df, path_column = path_column, label_column = label_column, img_width=img_width, img_height=img_height, color_mode="rgb")
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, stratify=y, random_state=seed)
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.15, stratify=y, random_state=seed)
-    #X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=(0.15/0.85), stratify=y_train, random_state=seed)
+    X, y, class_names = get_img_xy(df = df, path_column = path_column, label_column = label_column, img_width=img_width, img_height=img_height, color_mode="rgb", seed=seed,
+                                   keep_aspect_ratio = keep_aspect_ratio, random_flip=random_flip, random_brightness=random_brightness, random_contrast=random_contrast, random_hue=random_hue, random_saturation=random_saturation)
+    if validation_set:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.15, stratify=y, random_state=seed)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.15, stratify=y, random_state=seed)
+        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=(0.15/0.85), stratify=y_train, random_state=seed)
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, stratify=y, random_state=seed)
 
     # Preview Data Distribution
     show_labels_distribution(labels = y_train, class_names = class_names)
@@ -163,23 +171,29 @@ def preprocess_data(df, img_width, img_height, path_column, label_column, normal
     # Convert labels to one-hot vectors
     y_train_cat = tf.keras.utils.to_categorical(y_train, num_classes=len(class_names))
     y_test_cat = tf.keras.utils.to_categorical(y_test, num_classes=len(class_names))
-    #y_val_cat = tf.keras.utils.to_categorical(y_val, num_classes=len(class_names))
+    if validation_set:
+        y_val_cat = tf.keras.utils.to_categorical(y_val, num_classes=len(class_names))
 
     # Create a preprocessing layer for normalization
     if normalizer == False:
         normalization_layer = tf.keras.layers.Rescaling(1./255)
         X_train = normalization_layer(X_train)
         X_test = normalization_layer(X_test)
-        #X_val = normalization_layer(X_val)
+        if validation_set:
+            X_val = normalization_layer(X_val)
     else:
         X_train = normalizer(X_train)
         X_test = normalizer(X_test)
-        #X_val = normalizer(X_val)
+        if validation_set:
+            X_val = normalizer(X_val)
 
     # Review Shapes
     print("Training data shape X: ", X_train.shape, ", y:", y_train_cat.shape)
     print("Test data shape X: ", X_test.shape, ", y:", y_test_cat.shape)
-    #print("Validation data shape X: ", X_val.shape, ", y:", y_val_cat.shape)
-
-    #return {"train": X_train, "test": X_test, "val": X_val}, {"train": y_train, "test": y_test, "val": y_val}, {"train": y_train_cat, "test": y_test_cat, "val":y_val_cat}, class_names
-    return {"train": X_train, "test": X_test}, {"train": y_train, "test": y_test}, {"train": y_train_cat, "test": y_test_cat}, class_names
+    if validation_set:
+        print("Validation data shape X: ", X_val.shape, ", y:", y_val_cat.shape)
+    
+    if validation_set:
+        return {"train": X_train, "test": X_test, "val": X_val}, {"train": y_train, "test": y_test, "val": y_val}, {"train": y_train_cat, "test": y_test_cat, "val":y_val_cat}, class_names
+    else:
+        return {"train": X_train, "test": X_test}, {"train": y_train, "test": y_test}, {"train": y_train_cat, "test": y_test_cat}, class_names
